807369028a400445b42fc7ec922bf92f7b007afd
HOME STORE <2212407@dlu.edu.vn>
2024-09-10 10:41:22 +0700

    Add project files.

---
+++ QuanLyShopQuanAoTreEm/PAL/LoginForm.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace QuanLyShopQuanAoTreEm
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtPassWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picShow = new System.Windows.Forms.PictureBox();
+            this.picHide = new System.Windows.Forms.PictureBox();
+            this.picLoginIcon = new System.Windows.Forms.PictureBox();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.picShopIcon = new System.Windows.Forms.PictureBox();
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2Elipse2 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2HtmlToolTip1 = new Guna.UI2.WinForms.Guna2HtmlToolTip();
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoginIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShopIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnLogin.Location = new System.Drawing.Point(115, 363);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(209, 31);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // txtPassWord
+            // 
+            this.txtPassWord.Location = new System.Drawing.Point(115, 318);
+            this.txtPassWord.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPassWord.Name = "txtPassWord";
+            this.txtPassWord.Size = new System.Drawing.Size(174, 20);
+            this.txtPassWord.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(112, 302);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "PassWord :";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(115, 259);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(194, 20);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 244);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "UserName :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kid Clothes Shop Management | LogIn";
+            // 
+            // picShow
+            // 
+            this.picShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picShow.Image = global::QuanLyShopQuanAoTreEm.Properties.Resources.ShowPass;
+            this.picShow.Location = new System.Drawing.Point(292, 318);
+            this.picShow.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picShow.Name = "picShow";
+            this.picShow.Size = new System.Drawing.Size(27, 19);
+            this.picShow.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShow.TabIndex = 22;
+            this.picShow.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picShow, "Show PassWord");
+            this.picShow.Click += new System.EventHandler(this.picShow_Click);
+            // 
+            // picHide
+            // 
+            this.picHide.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picHide.Image = global::QuanLyShopQuanAoTreEm.Properties.Resources.HIdePass;
+            this.picHide.Location = new System.Drawing.Point(292, 318);
+            this.picHide.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picHide.Name = "picHide";
+            this.picHide.Size = new System.Drawing.Size(27, 19);
+            this.picHide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picHide.TabIndex = 21;
+            this.picHide.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picHide, "Hide PassWord");
+            this.picHide.Click += new System.EventHandler(this.picHide_Click);
+            // 
+            // picLoginIcon
+            // 
+            this.picLoginIcon.Image = global::QuanLyShopQuanAoTreEm.Properties.Resources.LoginIcon;
+            this.picLoginIcon.Location = new System.Drawing.Point(137, 55);
+            this.picLoginIcon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picLoginIcon.Name = "picLoginIcon";
+            this.picLoginIcon.Size = new System.Drawing.Size(150, 162);
+            this.picLoginIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLoginIcon.TabIndex = 20;
+            this.picLoginIcon.TabStop = false;
+            // 
+            // picClose
+            // 
+            this.picClose.Image = global::QuanLyShopQuanAoTreEm.Properties.Resources.Exit;
+            this.picClose.Location = new System.Drawing.Point(384, 10);
+            this.picClose.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(32, 49);
+            this.picClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picClose.TabIndex = 18;
+            this.picClose.TabStop = false;
+            this.guna2HtmlToolTip1.SetToolTip(this.picClose, "Close");
+            this.picClose.Click += new System.EventHandler(this.picClose_Click);
+            // 
+            // picShopIcon
+            // 
+            this.picShopIcon.Image = global::QuanLyShopQuanAoTreEm.Properties.Resources.LoginLogo_ico;
+            this.picShopIcon.Location = new System.Drawing.Point(3, 2);
+            this.picShopIcon.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.picShopIcon.Name = "picShopIcon";
+            this.picShopIcon.Size = new System.Drawing.Size(75, 81);
+            this.picShopIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShopIcon.TabIndex = 15;
+            this.picShopIcon.TabStop = false;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // guna2Elipse2
+            // 
+            this.guna2Elipse2.TargetControl = this.btnLogin;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.TargetControl = this.picClose;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2HtmlToolTip1
+            // 
+            this.guna2HtmlToolTip1.AllowLinksHandling = true;
+            this.guna2HtmlToolTip1.MaximumSize = new System.Drawing.Size(0, 0);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(425, 471);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassWord);
+            this.Controls.Add(this.picShow);
+            this.Controls.Add(this.picHide);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.picLoginIcon);
+            this.Controls.Add(this.picClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picShopIcon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kid Clothes Store Management System | Login Form";
+            ((System.ComponentModel.ISupportInitialize)(this.picShow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoginIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picShopIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtPassWord;
+        private System.Windows.Forms.PictureBox picShow;
+        private System.Windows.Forms.PictureBox picHide;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picLoginIcon;
+        private System.Windows.Forms.PictureBox picClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picShopIcon;
+        private Guna.UI2.WinForms.Guna2HtmlToolTip guna2HtmlToolTip1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse2;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+    }
+}
---
+++ QuanLyShopQuanAoTreEm/PAL/LoginForm.cs
@@ -0,0 +1,102 @@
+﻿using QuanLyShopQuanAoTreEm.PAL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLyShopQuanAoTreEm
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        public void EmptyBox()
+        {
+            txtUserName.Clear();
+            txtPassWord.Clear();
+        }
+        private void picShow_Click(object sender, EventArgs e)
+        {
+            if (picShow.Visible == true)
+            {
+                txtPassWord.UseSystemPasswordChar = false;
+                picShow.Visible = false;
+                picHide.Visible = true;
+            }
+        }
+
+        private void picClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void picHide_Click(object sender, EventArgs e)
+        {
+            if (picHide.Visible == true)
+            {
+                txtPassWord.UseSystemPasswordChar = true;
+                picShow.Visible = true;
+                picHide.Visible = false;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=HOANGPHUC\\SQLEXPRESS;Initial Catalog=KidShop_Login;Integrated Security=True;TrustServerCertificate=True");
+            con.Open();   
+            string query = "SELECT COUNT(*) FROM Login_From WHERE username=@username AND password=@password";
+            SqlCommand cmd =new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@username", txtUserName.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassWord.Text);
+            int count=(int)cmd.ExecuteScalar();
+            con.Close();
+            if (count > 0)
+            {
+                MessageBox.Show("Đăng Nhập Thành Công", " Thông Tin", MessageBoxButtons.OK,MessageBoxIcon.Information );
+
+            }
+            else
+            {
+                MessageBox.Show("lỗi Đăng Nhập");
+            }
+
+            //if (txtUserName.Text.Trim()== string.Empty)
+            //{
+            //    MessageBox.Show("Vui lòng nhập tên đăng nhập.", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    return;
+            //}
+            //else if (txtPassWord.Text.Trim() == string.Empty)
+            //{
+            //    MessageBox.Show("Vui lòng nhập mật khẩu.", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    return;
+            //}
+            //else
+            //{
+            //    bool chek = Computer.Computer.IsValidNamePass(txtUserName.Text.Trim(), txtPassWord.Text.Trim());
+            //    if (chek) 
+            //    {
+            //        FormMain formMain= new FormMain();
+            //        formMain.ShowDialog();
+            //        EmptyBox(); 
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác.", "Thông tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            //    }
+            //}  
+
+        }
+
+       
+    }
+}
